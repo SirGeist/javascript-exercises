9ac2be28c95b0a4b29c98a9f4b93c3282a0c22de
SirGeist <trinh1234533@yahoo.com>
2024-03-05 23:25:31 -0600

    05_sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
 
+  // If num1 is greater than num2,
+  // perform a swap
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
+  // Checks whether num1 or num2 are not ints or negatives
+  if (typeof sum != typeof num1 || typeof sum != typeof num2) {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  // Use the two given values to add up for the sum
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
