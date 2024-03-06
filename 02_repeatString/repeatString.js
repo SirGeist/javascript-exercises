3dc25d02fae47365ff31f98735b6bcce9a1715a1
SirGeist <trinh1234533@yahoo.com>
2024-03-05 21:01:30 -0600

    02-repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  let answer = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    answer += word;
+  }
+  return answer;
 };
 
 // Do not edit below this line
