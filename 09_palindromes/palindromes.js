d964ed50a22454c16258da49fae0d3c999d6101a
SirGeist <trinh1234533@yahoo.com>
2024-03-10 14:30:37 -0500

    09_palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+let isalnum = (char) => {
+  return /^[0-9a-zA-Z]+$/.test(char);
+};
 
+const palindromes = function (word) {
+  let left = 0;
+  let right = word.length - 1;
+
+  while (left <= right) {
+    while (!isalnum(word[left])) {
+      left++;
+    }
+    while (!isalnum(word[right])) {
+      right--;
+    }
+
+    if (word[left].toLowerCase() != word[right].toLowerCase()) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
