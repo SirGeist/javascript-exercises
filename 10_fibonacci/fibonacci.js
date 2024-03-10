dbdb747f30b653216453969e682533ad33e6025e
SirGeist <trinh1234533@yahoo.com>
2024-03-10 14:59:26 -0500

    10_fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+// Using memoization to reduce the amount of repeat information
+// when recursively adding the numbers
+const fibonacci = function (position, memo = {}) {
+  // Error handling
+  if (position < 0) {
+    return "OOPS";
+  }
 
+  // If we receive string input, turn to int
+  position = parseInt(position);
+  if (position <= 1) {
+    return position;
+  }
+
+  if (!memo[position]) {
+    return fibonacci(position - 1, memo) + fibonacci(position - 2, memo);
+  }
+
+  return memo[position];
 };
 
 // Do not edit below this line
